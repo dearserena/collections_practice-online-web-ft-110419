f5f9081fc8ef200df79baa31c514c6e52cfd4f8a
serena Lee <dearserenalee@gmail.com>
2019-11-19 08:38:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort do |left, right|
+    right<=>left
+end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end 
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  
+end 
+
+def kesha_maker(array)
+end 
+
+def find_a(array)
+end 
+
+def sum_array(array)
+end 
+
+def add_s(array)
+end 
